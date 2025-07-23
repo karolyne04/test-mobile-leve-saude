87975b0fa866fd4c5e139cc93d3e4af7b1406dbf
karolyne04 <ferreiracarolyne04@gmail.com>
2025-07-22 21:21:33 -0300

    refactor: configurações do Firebase com variáveis de ambiente

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -2,16 +2,25 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import {
+  FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID,
+} from '@env';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBJJd8av7Zs1AXv0z1zGxyoR7Urse8391E",
-  authDomain: "feedbackhub-83b2d.firebaseapp.com",
-  projectId: "feedbackhub-83b2d",
-  storageBucket: "feedbackhub-83b2d.firebasestorage.app",
-  messagingSenderId: "335214134775",
-  appId: "1:335214134775:web:95ea5cc4d0ed585d2b5af2"
+  apiKey:  FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket:FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
 };
 
+// export const firebaseApp = initializeApp(firebaseConfig);
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
