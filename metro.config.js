d70d95f7a4d715992e196af94e143fb5a4613f3c
karolyne04 <ferreiracarolyne04@gmail.com>
2025-07-26 19:26:14 -0300

    atulização local

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,27 @@
+// metro.config.js
 const { getDefaultConfig } = require('expo/metro-config');
 
+/** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
-module.exports = config; 
+// Força o Metro a tratar os pacotes @firebase/* como ESM
+config.resolver.resolveRequest = (context, moduleImport, platform) => {
+  if (moduleImport.startsWith('@firebase/')) {
+    return context.resolveRequest(
+      {
+        ...context,
+        isESMImport: true,
+      },
+      moduleImport,
+      platform
+    );
+  }
+
+  return context.resolveRequest(context, moduleImport, platform);
+};
+
+// (Opcional) Desativa suporte ao "exports" do package.json
+// Pode ajudar com alguns pacotes que causam conflito com ESM/CJS
+// config.resolver.unstable_enablePackageExports = false;
+
+module.exports = config;
--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,19 +1,18 @@
 // src/services/firebase.ts
 import { initializeApp } from "firebase/app";
-
-import AsyncStorage from "@react-native-async-storage/async-storage";
 import { getFirestore } from "firebase/firestore";
+import { getAuth, inMemoryPersistence, setPersistence } from "firebase/auth";
 
-import { getAuth } from "firebase/auth";
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
-import {
-  FIREBASE_API_KEY,
-  FIREBASE_AUTH_DOMAIN,
-  FIREBASE_PROJECT_ID,
-  FIREBASE_STORAGE_BUCKET,
-  FIREBASE_MESSAGING_SENDER_ID,
-  FIREBASE_APP_ID,
-} from "@env";
+// Importar variáveis de ambiente com fallback
+const FIREBASE_API_KEY = process.env.FIREBASE_API_KEY || "AIzaSyBJJd8av7Zs1AXv0z1zGxyoR7Urse8391E";
+const FIREBASE_AUTH_DOMAIN = process.env.FIREBASE_AUTH_DOMAIN || "feedbackhub-83b2d.firebaseapp.com";
+const FIREBASE_PROJECT_ID = process.env.FIREBASE_PROJECT_ID || "feedbackhub-83b2d";
+const FIREBASE_STORAGE_BUCKET = process.env.FIREBASE_STORAGE_BUCKET || "feedbackhub-83b2d.firebasestorage.app";
+const FIREBASE_MESSAGING_SENDER_ID = process.env.FIREBASE_MESSAGING_SENDER_ID || "335214134775";
+const FIREBASE_APP_ID = process.env.FIREBASE_APP_ID || "1:335214134775:web:95ea5cc4d0ed585d2b5af2";
 
 const firebaseConfig = {
   apiKey: FIREBASE_API_KEY,
@@ -27,7 +26,14 @@ const firebaseConfig = {
 // Inicializa o app
 const app = initializeApp(firebaseConfig);
 
-const auth = getAuth(app);
+// Inicializa o auth
+
+const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+});
+
+
+
 const db = getFirestore(app);
 
 export { auth, db };
